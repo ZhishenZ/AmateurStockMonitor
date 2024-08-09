d11c76a8d31003863826ebc6fb1f81b30d8caf4e
ZhishenZ <clemenszhang@gmail.com>
2024-08-09 16:48:41 +0200

    Add HTML template

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from config import app, setup_db
+from config import app, setup_db, Stock, Indicator
+from flask import render_template
 
 setup_db()
 
-@app.route('/')
+
+def load_stock_data():
+    stocks = Stock.query.all()
+    indicator_header = set()
+    for stock in stocks:
+        for indicator in stock.indicators:
+            indicator_header.add(indicator.indicator_type)
+    indicator_header = sorted(indicator_header)
+
+    return stocks, indicator_header
+
+
+@app.route("/")
 def index():
-    return "The database is set up."
+    stocks, indicator_header = load_stock_data()
+
+    return render_template(
+        "index.html", stocks=stocks, indicator_header=indicator_header
+    )
+
 
 if __name__ == "__main__":
     app.run()
